199d1bf885f9228004fd3d6cbb8ecb02bc92f2d0
WithLithum <WithLithum@foxmail.com>
2023-01-19 12:19:49 +0800

    ✨ Exception helper

---
+++ NexusKrop.IceCube/Exceptions/ExceptionHelper.cs
@@ -0,0 +1,37 @@
+﻿namespace NexusKrop.IceCube.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class ExceptionHelper
+{
+    /// <summary>
+    /// Throws <see cref="FileNotFoundException"/> if the specified file does not exist.
+    /// </summary>
+    /// <param name="fileName">The name of the file.</param>
+    /// <exception cref="FileNotFoundException">The file specified does not exist.</exception>
+    public static void ThrowIfFileNotExist(string fileName)
+    {
+        if (!File.Exists(fileName))
+        {
+            throw new FileNotFoundException(string.Format(ExceptionHelperResources.FileNotFound,
+                fileName), fileName);
+        }
+    }
+
+    /// <summary>
+    /// Throws <see cref="DirectoryNotFoundException"/> if the specified directory does not exist.
+    /// </summary>
+    /// <param name="directoryName">The name of the directory.</param>
+    /// <exception cref="DirectoryNotFoundException">The directory specified does not exist.</exception>
+    public static void ThrowIfDirectoryNotExist(string directoryName)
+    {
+        if (!Directory.Exists(directoryName))
+        {
+            throw new DirectoryNotFoundException(string.Format(ExceptionHelperResources.DirectoryNotFound,
+                directoryName));
+        }
+    }
+}
