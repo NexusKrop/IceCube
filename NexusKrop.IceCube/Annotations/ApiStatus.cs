dbb0018c65c670ab75727444e51a40ed4d2525e7
WithLithum <WithLithum@outlook.com>
2023-03-03 10:43:16 +0800

    ✨ (misc) API Status Attribute

---
+++ NexusKrop.IceCube/Annotations/ApiStatus.cs
@@ -0,0 +1,20 @@
+﻿namespace NexusKrop.IceCube.Annotations;
+
+/// <summary>
+/// Specifies the status of the API.
+/// </summary>
+public enum ApiStatus
+{
+    /// <summary>
+    /// Indicates that the API is stable and may be used for production.
+    /// </summary>
+    Stable,
+    /// <summary>
+    /// Indicates that the API is not stable and should not be used for production yet.
+    /// </summary>
+    Unstable,
+    /// <summary>
+    /// Indicates that the API is in rapid development and must not be used for production.
+    /// </summary>
+    Alpha
+}
---
+++ NexusKrop.IceCube/Annotations/ApiStatusAttribute.cs
@@ -0,0 +1,24 @@
+﻿namespace NexusKrop.IceCube.Annotations;
+using System;
+
+/// <summary>
+/// Specifies the status of an API. This class cannot be inherited.
+/// </summary>
+[AttributeUsage(AttributeTargets.All, AllowMultiple = false)]
+public sealed class ApiStatusAttribute :
+    Attribute
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ApiStatusAttribute"/> class.
+    /// </summary>
+    /// <param name="status">The status.</param>
+    public ApiStatusAttribute(ApiStatus status)
+    {
+        Status = status;
+    }
+
+    /// <summary>
+    /// Gets or sets the status of the API specified.
+    /// </summary>
+    public ApiStatus Status { get; set; }
+}
