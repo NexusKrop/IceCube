a01183dee1b3fb934361cb15bad292e7962c1100
WithLithum <WithLithum@outlook.com>
2023-06-09 21:26:54 +0800

    ✨ (util) URL Combine feature

---
+++ NexusKrop.IceCube.Tests/UrlPathTests.cs
@@ -0,0 +1,14 @@
+﻿namespace NexusKrop.IceCube.Tests;
+
+using NexusKrop.IceCube.Util;
+
+public class UrlPathTests
+{
+    [Test]
+    public void Combine_CompletePath()
+    {
+        var combineResult = UrlPath.Combine("https://", "example.com", "shutter");
+
+        Assert.That(combineResult, Is.EqualTo("https://example.com/shutter/"));
+    }
+}
---
+++ NexusKrop.IceCube/Util/UrlPath.cs
@@ -0,0 +1,30 @@
+﻿namespace NexusKrop.IceCube.Util;
+using System.Text;
+
+/// <summary>
+/// Provide utilities to manipulate URL paths.
+/// </summary>
+public static class UrlPath
+{
+    /// <summary>
+    /// Combines two or more URL paths to a single URL path.
+    /// </summary>
+    /// <param name="parts">The parts of the URL path to combine.</param>
+    /// <returns>The combined result.</returns>
+    public static string Combine(params string[] parts)
+    {
+        var builder = new StringBuilder();
+
+        foreach (var part in parts)
+        {
+            builder.Append(part);
+
+            if (!part.EndsWith("/"))
+            {
+                builder.Append('/');
+            }
+        }
+
+        return builder.ToString();
+    }
+}
