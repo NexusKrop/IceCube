9aaadea9577184c7288548fb9d08c24ac4cb1d38
WithLithum <WithLithum@outlook.com>
2023-06-19 13:32:07 +0800

    ✨ (general) Additional collections utilities

---
+++ NexusKrop.IceCube/Collections/Dictionary.cs
@@ -0,0 +1,28 @@
+﻿namespace NexusKrop.IceCube.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Provides utilities for manipulating dictionaries.
+/// </summary>
+public static class Dictionary
+{
+    /// <summary>
+    /// Creates a dictionary from the specified list of Key-Value pairs.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the keys in the dictionary.</typeparam>
+    /// <typeparam name="TValue">The type of the values in the dictionary.</typeparam>
+    /// <param name="pairs">The pairs to create dictionary from.</param>
+    /// <returns>A new instance of dictionary.</returns>
+    public static IDictionary<TKey, TValue> From<TKey, TValue>(IEnumerable<KeyValuePair<TKey, TValue>> pairs)
+        where TKey : notnull
+    {
+        var result = new Dictionary<TKey, TValue>();
+
+        foreach (var pair in pairs)
+        {
+            result.Add(pair.Key, pair.Value);
+        }
+
+        return result;
+    }
+}
---
+++ NexusKrop.IceCube/Collections/List.cs
@@ -0,0 +1,30 @@
+﻿namespace NexusKrop.IceCube.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Provides methods to assist in creation of lists.
+/// </summary>
+public static class List
+{
+    /// <summary>
+    /// Creates a new list consisting of the specified <paramref name="values"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of the list.</typeparam>
+    /// <param name="values">The values.</param>
+    /// <returns>A list consisting of the <paramref name="values"/>.</returns>
+    public static IList<T> Of<T>(params T[] values)
+    {
+        return new List<T>(values);
+    }
+
+    /// <summary>
+    /// Creates a new list consisting of the specified <paramref name="value"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of the list.</typeparam>
+    /// <param name="value">The value.</param>
+    /// <returns>A list consisting of the <paramref name="value"/>.</returns>
+    public static IList<T> Of<T>(T value)
+    {
+        return new List<T>() { value };
+    }
+}
